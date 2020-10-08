11bcc8b7304a4bedb04436ced11a485e3562632c
LuisBenavides <luisbv@tec.mx>
2020-10-08 08:34:06 -0500

    Update UnivariateObtainAverageRunLength.R

--- R/UnivariateObtainAverageRunLength.R
+++ R/UnivariateObtainAverageRunLength.R
@@ -303,7 +303,7 @@ getARL.test <- function(n, m, theta = NULL, Ftheta = NULL,
     }
   }
   RLs <- NULL
-  stop.times = NA
+  stop.times <- 0
   if (isParallel) {
     cluster <- parallel::makeCluster(parallel::detectCores() - 1)
     parallel::clusterExport(cluster, "NS.test")
@@ -314,7 +314,7 @@ getARL.test <- function(n, m, theta = NULL, Ftheta = NULL,
 
   } else {
     t0 <- Sys.time()
-    stop.times <- 0
+
     for (r in 1:replicates) {
       RL <- SNS.test::getRL.test(1, n = n, m = m, theta = theta, Ftheta = Ftheta, dist = dist, mu = mu, sigma = sigma, dist.par = dist.par, chart = chart, chart.par = chart.par, calibrate = calibrate, arl0 = arl0, alignment=alignment, constant=constant,absolute=absolute,isFixed=isFixed,scoring=scoring,Chi2corrector=Chi2corrector, rounding.factor = rounding.factor,tie.correction =tie.correction, stop.times = stop.times, replicates = r)
       if(RLs == arl0  * 15){
